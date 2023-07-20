83a38d45adc12d505d485e334428218f4aefd617
RenzoSuganuma <suganumarenzo2004@gmail.com>
2023-07-20 19:41:35 +0900

    no message

---
+++ Assets/Scenes/230718/HelpingShin.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class HelpingShin : MonoBehaviour
+{
+    Rigidbody2D rd2d = default;
+}
