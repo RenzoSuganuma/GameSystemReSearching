be97c83916ffc2c12fa59494c2fd091919454d87
RenzoSuganuma <suganumarenzo2004@gmail.com>
2023-08-11 23:49:09 +0900

    コードをちょろっと書いた

--- Assets/Scenes/UIToolKitPract/DGWUISystem.cs
+++ Assets/Scenes/UIToolKitPract/DGWUISystem.cs
@@ -3,99 +3,129 @@ using UnityEngine;
 using UnityEngine.UIElements;
 namespace DiscoveryGamesUISystem
 {
+    interface IInterface
+    {
+        public void SetVisible(bool visible);
+        public bool GetVisible();
+    }
+
     public class DGWUISystem { }
     /// <summary>
     /// UIToolKitのLabelを扱うときにインスタンス化して使う
     /// </summary>
-    public class UILabel
+    public class UILabel : IInterface
     {
         /// <summary> UIToolKit.Label をつかって初期化する </summary>
         Label _label;
         public UILabel(VisualElement root, string uiName)
         {
-            this._label = root.Q<Label>(uiName);
-            this._label.text = "LABEL...";
+            _label = root.Q<Label>(uiName);
+            _label.text = "LABEL...";
         }
-        /// <summary> 可視性の設定 </summary>
         public void SetVisible(bool visible)
         {
-            this._label.visible = visible;
+            _label.visible = visible;
         }
-        /// <summary> 可視性の取得 </summary>
         public bool GetVisible()
         {
-            return this._label.visible;
-        }/// <summary> テキストの設定 </summary>
+            return (_label.visible);
+        }
         public void SetText(string text)
         {
-            this._label.text = text;
+            _label.text = text;
         }
     }
     /// <summary>
     /// UIToolKitのButtonを扱うときにインスタンス化して使う
     /// </summary>
-    public class UIButton
+    public class UIButton : IInterface
     {
         /// <summary> UIToolKit.Button をつかって初期化する </summary>
         Button _button;
         public UIButton(VisualElement root, string uiName)
         {
             _button = root.Q<Button>(uiName);
-            this._button.text = "BUTTON...";
+            _button.text = "BUTTON...";
         }
-        /// <summary> 可視性の設定 </summary>
         public void SetVisible(bool visible)
         {
-            this._button.visible = visible;
+            _button.visible = visible;
         }
-        /// <summary> 可視性の取得 </summary>
         public bool GetVisible()
         {
-            return this._button.visible;
+            return (_button.visible);
         }
         /// <summary> ボタンクリック時に呼び出される関数の登録 </summary>
         public void AddButtonHandler(Action buttonHandler)
         {
-            this._button.clicked += buttonHandler;
+            _button.clicked += buttonHandler;
         }
         /// <summary> ボタンクリック時に呼び出される関数の登録解除 </summary>
         public void RemoveButtonHandler(Action buttonHandler)
         {
-            this._button.clicked -= buttonHandler;
+            _button.clicked -= buttonHandler;
         }
     }
     /// <summary>
+    /// UIToolKitのToggleを扱うときにインスタンス化して使う
+    /// </summary>
+    public class UIToggle : IInterface
+    {
+        /// <summary> UIToolKit.Toggle をつかって初期化する </summary>
+        Toggle _toggle;
+        public UIToggle(VisualElement root, string uiName)
+        {
+            _toggle = root.Q<Toggle>(uiName);
+            _toggle.text = "TOGGLE...";
+        }
+        public bool GetVisible()
+        {
+            return (_toggle.visible);
+        }
+        public void SetVisible(bool visible)
+        {
+            _toggle.visible = visible;
+        }
+        /// <summary> Toggleの値をbool型で返す </summary>
+        /// <returns></returns>
+        public bool GetToggle()
+        {
+            return (_toggle.value);
+        }
+    }
+
+    /// <summary>
     /// UIToolKitのProgressBarを扱うときにインスタンス化して使う
     /// </summary>
-    public class UIProgBar
+    public class UIProgBar : IInterface
     {
         /// <summary> UIToolKit.ProgressBar をつかって初期化する </summary>
         ProgressBar _bar;
         public UIProgBar(VisualElement root, string uiName)
         {
-            this._bar = root.Q<ProgressBar>(uiName);
-            this._bar.title = "PROGBAR...";
+            _bar = root.Q<ProgressBar>(uiName);
+            _bar.title = "PROGBAR...";
         }
         /// <summary> 可視性の設定 </summary>
         public void SetVisible(bool visible)
         {
-            this._bar.visible = visible;
+            _bar.visible = visible;
         }
         /// <summary> 可視性の取得 </summary>
         public bool GetVisible()
         {
-            return this._bar.visible;
+            return _bar.visible;
         }
         /// <summary> 値のとる範囲の設定 </summary>
         public void SetRangeOfValue(int start, int end)
         {
-            this._bar.lowValue = start;
-            this._bar.highValue = end;
+            _bar.lowValue = start;
+            _bar.highValue = end;
         }
         /// <summary> 値の設定 </summary>
         public void SetProgValue(int value)
         {
-            this._bar.value = value;
+            _bar.value = value;
         }
     }
 }
