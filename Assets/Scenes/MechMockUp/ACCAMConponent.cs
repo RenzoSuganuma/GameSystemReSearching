ad433c9baced0e06c84a134dad0753c8586e058a
RenzoSuganuma <discoverygworks2023@gmail.com>
2023-09-28 13:20:49 +0900

    オクルージョン

--- Assets/Scenes/MechMockUp/ACCAMConponent.cs
+++ Assets/Scenes/MechMockUp/ACCAMConponent.cs
@@ -1,8 +1,7 @@
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using Unity.VisualScripting;
 using UnityEngine;
+using RuntimeLog;
+[RequireComponent(typeof(SphereCollider))]
 public class ACCAMComponentAlpha : MonoBehaviour
 {
     [SerializeField] Transform _target;
@@ -11,12 +10,22 @@ public class ACCAMComponentAlpha : MonoBehaviour
     [SerializeField] float _camHeight;
     [SerializeField] float _rotateRadius;
     [SerializeField] float _camLockOnDistance;
+    SphereCollider _sphereCollider;
+    RuntimeLogComponent _logComponent;
     float _thetaX = 0;
     float _thetaY = 0;
     void Start()
     {
-        //NULLだったら何もしない
-        if (GetComponent<Camera>() == null) return;
+        //ログコンポーネントのインスタンス化
+        _logComponent = new(new Rect(0, 0, 100, 100));
+        //NULLだったら警告ログを吐き出す
+        if (GetComponent<Camera>() == null) Debug.LogWarning("プレイヤーカメラが見つからない");
+        if (!TryGetComponent<SphereCollider>(out _sphereCollider)) Debug.LogWarning("球コライダーが見つからない");
+        //コンポーネント値初期化(球コライダー)
+        _sphereCollider.isTrigger = true;
+        _sphereCollider.providesContacts = true;
+        _sphereCollider.radius = _camLockOnDistance;
+        //ターゲットを向く
         this.transform.rotation =
             Quaternion.LookRotation(_target.transform.position - this.transform.position
             , Vector3.up);
@@ -39,14 +48,22 @@ public class ACCAMComponentAlpha : MonoBehaviour
         , Vector3.up);
         //ロックオン範囲内のオブジェクトを検索
     }
-    private void OnTriggerEnter(Collider other)
+    private void OnGUI()
     {
+        //ログの描写表示
+        _logComponent.DisplayLog("ログ出力テスト");
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (!other.CompareTag("Player"))
+            other.gameObject.SetActive(false);
     }
     private void OnTriggerExit(Collider other)
     {
     }
     private void OnDrawGizmos()
     {
+        //補足距離の球メッシュ描写
         Gizmos.color = Color.green;
         Gizmos.DrawWireSphere(this.transform.position, _camLockOnDistance);
     }
