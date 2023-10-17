8197ef11236593dcc6cd9baef0e06d05c8ad2f79
RenzoSuganuma <discoverygworks2023@gmail.com>
2023-10-17 16:07:18 +0900

    辞書実装中

--- Assets/Scenes/Refrection/DataDictionary.cs
+++ Assets/Scenes/Refrection/DataDictionary.cs
@@ -8,33 +8,59 @@ using System.Collections;
 */
 [Serializable]
 /// <summary> 特定の登録名に対応した値を格納するための機能を提供するクラス </summary>
-public class PropertyDataBaseClass<TDataKey, TDataValue>
+public class DataDictionary<TDataKey, TDataValue>
 {
     //List<DataPair<文字列プロパティ登録名型,object型のプロパティ値保持用の型>>
     //監視対象のプロパティの登録名と名前のデータベース
-    DataPair<TDataKey, TDataValue>[] _coreDataBase, _tempDataBase;
+    DataPair<TDataKey, TDataValue>[] _coreDataBase;
     public event Action OnDataAdded;
     public event Action OnDataRemoved;
-    public PropertyDataBaseClass()//コンストラクタ
+    public DataDictionary()//コンストラクタ
     {
         _coreDataBase = new DataPair<TDataKey, TDataValue>[0];//データベースのインスタンス化
-        _tempDataBase = _coreDataBase;
     }
-    ~PropertyDataBaseClass()//デコンストラクタ
+    ~DataDictionary()//デコンストラクタ
     {
         _coreDataBase = null;
+        OnDataAdded = null;
+        OnDataRemoved = null;
     }
+    /// <summary> データの登録 </summary>
+    /// <param name="key"></param>
+    /// <param name="value"></param>
     public void Add(TDataKey key, TDataValue value)
     {
-        var len = _coreDataBase.Length;
-        Array.Resize<DataPair<TDataKey, TDataValue>>(ref _coreDataBase, len + 1);
+        for (int i = 0; i < _coreDataBase.Length; i++)
+        {
+            if (_coreDataBase[i].Key.Equals(key))
+            {
+                throw new Exception("It Seems Already Appended Same Key Value");
+            }
+        }//Search Same Key Value
+        Array.Resize<DataPair<TDataKey, TDataValue>>(ref _coreDataBase, _coreDataBase.Length + 1);
         //Append Element
-        _coreDataBase[len] = new DataPair<TDataKey, TDataValue>(key, value);
+        _coreDataBase[_coreDataBase.Length - 1] = new DataPair<TDataKey, TDataValue>(key, value);
         if (OnDataAdded != null) OnDataAdded();
     }
+    /// <summary> データの登録解除 </summary>
+    /// <param name="key"></param>
+    /// <param name="value"></param>
     public void Remove(TDataKey key, TDataValue value)
     {
-        
+        for (int i = 0; i < _coreDataBase.Length; i++)
+        {
+            if (_coreDataBase[i].Key.Equals(key)//If The Pair Found
+                && _coreDataBase[i].Value.Equals(value))
+            {
+                _coreDataBase[i] = null;//Erase Element
+                for (int j = i; j < _coreDataBase.Length; j++)
+                {
+                    _coreDataBase[j] = _coreDataBase[(j + 1 < _coreDataBase.Length) ? j + 1 : j];
+                    _coreDataBase[(j + 1 < _coreDataBase.Length) ? j + 1 : j] = null;
+                    Array.Resize<DataPair<TDataKey, TDataValue>>(ref _coreDataBase, _coreDataBase.Length - 1);
+                }//Make No Gap
+            }
+        }//Serach Pairs
         if (OnDataAdded != null) OnDataRemoved();
     }
 }
--- Assets/Scenes/Refrection/PropertyObserver.cs
+++ Assets/Scenes/Refrection/PropertyObserver.cs
@@ -10,9 +10,18 @@ using Unity.VisualScripting;
 */
 public class PropertyObserver : MonoBehaviour
 {
-    [SerializeField] PropertyDataBaseClass<string, object> _dataBase = new();
-    private void Start()
+    [SerializeField] DataDictionary<string, object> _dataBase = new();
+    private void Awake()
     {
         _dataBase.Add("Test", (object)100);
+        _dataBase.Add("Test_", (object)100);
+    }
+    private void Start()
+    {
+        //_dataBase.Remove("Test", (object)100);
+    }
+    private void OnDisable()
+    {
+        _ = _dataBase;
     }
 }
