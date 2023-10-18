17a9a4afb8c96c28b57b660bf005a0f1c7090c3f
RenzoSuganuma <discoverygworks2023@gmail.com>
2023-10-19 01:35:11 +0900

    オブザーバークラス開発途中

--- Assets/Scenes/Refrection/PropInfoHandlingInfra/PropInfoObserver.cs
+++ Assets/Scenes/Refrection/PropInfoHandlingInfra/PropInfoObserver.cs
@@ -6,7 +6,17 @@ public interface IPropInfoObserver
     PropertyInfoHandlerLinker PropertyInfoHandlerLinker { get; set; }
     List<string> ObserveTargetResistList { get; set; }
 }
-public class PropInfoObserver : MonoBehaviour
+public class PropInfoObserver : MonoBehaviour, IPropInfoObserver
 {
-    
+    public PropertyInfoHandlerLinker PropertyInfoHandlerLinker { get; set; }
+    public List<string> ObserveTargetResistList { get; set; } = new();
+    private void Update()
+    {
+        
+    }
+    public void PropInfoObserverSetup(PropertyInfoHandlerLinker propInfoHandlerLinker, List<string> observeTargetResistList)
+    {
+        this.PropertyInfoHandlerLinker = propInfoHandlerLinker;
+        this.ObserveTargetResistList = observeTargetResistList;
+    }
 }
