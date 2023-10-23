881b1c8c791ed467d5dca2155e2e52af11f964b0
RenzoSuganuma <discoverygworks2023@gmail.com>
2023-10-23 23:33:39 +0900

    no message

--- Assets/Scenes/OriginalAIBehaviours/AI_Dev_alpha.cs
+++ Assets/Scenes/OriginalAIBehaviours/AI_Dev_alpha.cs
@@ -1,13 +1,14 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
-using UnityEngine.Events;
 /// <summary> 汎用ゲーム用AIの機能を提供するクラス </summary>
 /// 設計思想：次にとるビヘイビアをキューに追加し、それを実行し終わったなら、キューから削除する
 
 // キューにセンドするビヘイビアはある程度定まっているか、特定のビヘイビアにはこのビヘイビアを...とアタッチできるように
 // UnityEventを割り当ててやるのも１つのアプローチだろう。
+[RequireComponent(typeof(Rigidbody))]
 public class AI_Dev_alpha : MonoBehaviour
 {
     // Idle
@@ -15,13 +16,34 @@ public class AI_Dev_alpha : MonoBehaviour
     // Patroll
     protected Action OnPatroll;
     // Chase
+    protected Action OnChase;
     // Attack
+    protected Action OnAttack;
+
+    [SerializeField] float _moveSpeed;
+    [SerializeField] int _targettingSightRangeRadius;
+    [SerializeField] int _attackingRangeRadius;
+    [SerializeField] int _targetLayerNumber;
+
+    Rigidbody _rb;
+    public List<GameObject> _targetObjects = new();
+    public int _targetObjIndex = 0;
+    public bool _isFound = false;
+    public bool _isCanAttack = false;
     private void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
+        _targetObjects = GameObject.FindObjectsOfType<GameObject>().Where(x => x.layer == _targetLayerNumber).ToList();
     }
     private void FixedUpdate()
     {
         
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, _targettingSightRangeRadius);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, _attackingRangeRadius);
     }
 }
